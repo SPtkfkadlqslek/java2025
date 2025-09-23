7e3fc385be6516c0cb5c1a7e59af224b4d9812b2
김세훈 <sehun62266@gmail.com>
2025-09-23 10:49:25 +0900

    Add files via upload

---
+++ sep23.java
@@ -0,0 +1,19 @@
+package scnu;
+
+public class sep23 {
+
+	public static void main(String[] args) {
+			int a;
+			int b;
+			
+			String str1 = null;
+			String str2 = str1;
+			
+			System.out.println("HI.");	
+			
+//			int[] intArray = null;
+//			intArray[0] = 10;
+
+	}
+
+}
---
+++ sep23_2.java
@@ -0,0 +1,19 @@
+package scnu;
+
+public class sep23_2 {
+
+	public static void main(String[] args) {
+//			int[] intArray = new int [10];
+//			
+//			for(int i=0; i<10; i++) {
+//				System.out.println(intArray[i]);
+		
+			int[] [] scores= new int[2] [3];
+			scores[0] = new int[3];
+			
+			}
+			
+
+	}
+
+}
