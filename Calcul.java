2a7a5ae587235b93dab828fdbb3ecd01cf10b200
김세훈 <sehun62266@gmail.com>
2025-09-30 23:57:58 +0900

    Add files via upload

---
+++ Calcul.java
@@ -0,0 +1,12 @@
+package sec03.exam01;
+
+public class Calcul {
+	int addInt(int a, int b)
+	{
+		return a + b;
+	}
+	double add(double a, double b)
+	{
+		return a+b;
+	}
+}
---
+++ student.java
@@ -0,0 +1,24 @@
+package sec03.exam01;
+
+public abstract class student {
+	int studentNumber;
+	String name;
+	double midScore;
+	double finalScore;
+	double projectScore;
+	public void SayHello() {
+		System.out.println("Hello !");
+		
+	}
+	
+	public void SetMidScore()
+	{
+		
+	}
+	
+	public void TotalScore()
+	{
+	}
+
+
+}
